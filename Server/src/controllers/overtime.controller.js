513c85c5a632e11a3558e5ae1b2ccc4bb9a30430
Chris <chris_db@icloud.com>
2019-10-21 10:27:13 -0600

    controlador de horas extra

---
+++ Server/src/controllers/overtime.controller.js
@@ -0,0 +1,76 @@
+import mysqlConnection from '../database.key'
+
+export async function nuevaHorasExtra(req, res){
+    const {_cedula, _cantidad_horas,_motivo, _fecha } = req.body
+    const query = `
+        SET @_cedula = ?;
+        SET @_cantidad_horas = ?;
+        SET @_motivo = ?;
+        SET @_fecha =?;
+        CALL nuevaHoraExtra(@_cedula, @_cantidad_horas, @_motivo, @_fecha);
+    `
+    await mysqlConnection.query('SELECT cedula FROM empleados WHERE cedula = ?', [_cedula], (err, rows, fields)=>{
+        if(!err && rows.length > 0){
+            mysqlConnection.query(query, [_cedula, _cantidad_horas, _motivo, _fecha], (err, rows, fields)=>{
+                !err ? res.json({Status: "OK"}) : res.json({"Message": err})
+            })
+        }else{
+            res.json({"Message": err})
+        }
+    })
+}
+export async function horasExtraEmpleado(req, res){
+    const {_cedula} = req.params
+    const query = `
+        SELECT a.cedula_empleado, b.p_apellido, b.nombre, a.cantidad_horas, a.motivo, a.fecha 
+        FROM horas_extra a
+        INNER JOIN empleados b
+        ON a.cedula_empleado = b.cedula
+        WHERE a.cedula_empleado = ${_cedula};
+    `
+    await mysqlConnection.query('SELECT cedula FROM empleados WHERE cedula = ?', [_cedula], (err, rows, fields)=>{
+        if(!err && rows.length > 0){
+            mysqlConnection.query(query, [_cedula], (err, rows, fields)=>{
+                !err ? res.json(rows) : res.json({"Message": err})
+            })
+        }else{
+            res.json({"Message": err})
+        }
+    })
+}
+export async function actualizarHorasExtra(req, res){
+    const {_id, _cedula, _cantidad_horas, _motivo, _fecha} = req.body
+    const query = `
+        SET @_id = ?;
+        SET @_cedula = ?;
+        SET @_cantidad_horas = ?;
+        SET @_motivo = ?;
+        SET @_fecha = ?;
+        CALL actualizarHorasExtra(@_id, _@cedula, @_cantidad_horas, @_motivo, @_fecha);
+    `
+    await mysqlConnection.query('SELECT id FROM horas_extra WHERE id = ? AND activo = true', [_id], (err, rows, fields)=>{
+        if(!err && rows.length > 0){
+            mysqlConnection.query(query, [_id, _cedula, _cantidad_horas, _motivo, _fecha ], (err, rows, fields)=>{
+                !err ? res.json({Status: "OK"}) : res.json({"Message": err})
+            })
+        }else{
+            res.json({"Message": err})
+        }
+    })
+}
+export async function borrarHorasExtra(req, res){
+    const {_id} = req.params
+    const query = `
+        SET @_id = ?;
+        CALL eliminarHoraExtra(@_id);
+    `
+    await mysqlConnection.query('SELECT id FROM horas_extra WHERE id = ? AND activo = true', [_id], (err, rows, fields)=>{
+        if (!err && rows.length > 0) {
+            mysqlConnection.query(query, [_id], (err, rows, fields)=>{
+                !err ? res.json({Status: "OK"}) : res.json({"Message": err})
+            })
+        } else {
+            res.json({"Message": err})
+        }
+    })
+}
---
+++ Server/src/routes/overtime.js
@@ -0,0 +1,4 @@
+import {Router} from 'express'
+import {  } from '../controllers/overtime.controller'
+
+const router = Router()
