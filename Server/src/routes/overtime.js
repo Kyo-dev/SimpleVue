b8b47e50b8ab40e00b7d866689261159500b6221
Chris <chris_db@icloud.com>
2019-10-21 10:53:24 -0600

    ruta de horas extra

--- Server/src/routes/overtime.js
+++ Server/src/routes/overtime.js
@@ -1,4 +1,9 @@
 import {Router} from 'express'
-import {  } from '../controllers/overtime.controller'
-
+import { nuevaHorasExtra, horasExtraEmpleado, actualizarHorasExtra, borrarHorasExtra } from '../controllers/overtime.controller'
 const router = Router()
+
+router.post('/horas-extra', nuevaHorasExtra)
+router.get('/horas-extra/:_cedula', horasExtraEmpleado)
+router.put('/horas-extra', actualizarHorasExtra)
+router.delete('/horas-extra/:_id', borrarHorasExtra)
+export default router
