9bff1035f5a4254610626294d05d76e01a3f9354
Chris <chris_db@icloud.com>
2019-11-05 10:05:52 -0600

    -

--- client/src/views/Employee.vue
+++ client/src/views/Employee.vue
@@ -1,56 +1,21 @@
 <template>
-  <v-card color="basil">
-    <v-card-title class="text-center justify-center py-6">
-      <h1 class="font-weight-bold display-3 basil--text" color="primary">Empleados</h1>
-    </v-card-title>
-    <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-        <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
-      </v-tabs>
-    </v-tabs>
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
-        <v-card flat>
-          <newEmployee />
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <v-card-text>
-            <allEmployee />
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
+  <div>
+    <Employee/>
+  </div>
 </template>
 
 <script>
-import newEmployee from "../components/Employee_new.component.vue";
-import allEmployee from "../components/Employee_all.component.vue";
+import Employee from "../components/Employee.component.vue";
 export default {
   data() {
-    return {
-      tab: [],
-      items: ["Nuevo empleado", "Todos los empleados"],
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      text2: "hola mundo"
-    };
+    return {};
   },
   components: {
-    newEmployee,
-    allEmployee
+    Employee
   }
 };
 </script>
 
 <style>
-/* Helper classes */
-.basil {
-  background-color: #fffbe6 !important;
-}
-.basil--text {
-  color: #356859 !important;
-}
+
 </style>
